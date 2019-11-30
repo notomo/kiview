eaf836ac9a9baa27e0c9a59c66e3d538724b44ac
notomo <notomo.motono@gmail.com>
2019-11-30 16:51:33 +0900

    Separate repository impl

--- src/src/repository/mod.rs
+++ src/src/repository/mod.rs
@@ -1,5 +1,8 @@
 mod path;
-pub use path::{Dispatcher, FilePathRepository, FullPath, PathRepository};
+pub use path::{Dispatcher, FullPath, Path, PathRepository};
+
+mod file;
+pub use file::{FilePath, FilePathRepository};
 
 mod error;
 pub use error::{Error, ErrorKind};
--- src/src/repository/path.rs
+++ src/src/repository/path.rs
@@ -1,87 +1,4 @@
-use std::fs;
-
-pub trait PathRepository {
-    fn list(&self, path: &str) -> Result<Vec<FullPath>, crate::repository::Error>;
-    fn create(&self, path: &str) -> Result<(), crate::repository::Error>;
-}
-
-pub struct FilePathRepository {}
-
-impl PathRepository for FilePathRepository {
-    fn list(&self, path: &str) -> Result<Vec<FullPath>, crate::repository::Error> {
-        let parent_directory = vec![FullPath {
-            name: String::from(".."),
-            path: std::path::Path::new(&path)
-                .parent()
-                .unwrap_or_else(|| std::path::Path::new(&path))
-                .canonicalize()?
-                .to_str()?
-                .to_string(),
-            is_parent_node: true,
-        }];
-
-        let paths: Vec<_> = fs::read_dir(path)?
-            .filter(|result| result.is_ok())
-            .map(|result| result.unwrap().path())
-            .collect();
-
-        let directories: Vec<_> = paths
-            .iter()
-            .filter(|p| p.is_dir())
-            .map(|p| FullPath {
-                name: p
-                    .file_name()
-                    .and_then(|name| name.to_str().and_then(|name| Some(format!("{}/", name))))
-                    .unwrap_or(String::from("")),
-                path: p
-                    .canonicalize()
-                    .and_then(|p| Ok(p.to_str().unwrap_or("").to_string()))
-                    .unwrap_or(String::from("")),
-                is_parent_node: false,
-            })
-            .filter(|p| p.name != "" && p.path != "")
-            .collect();
-
-        let files: Vec<_> = paths
-            .iter()
-            .filter(|p| !p.is_dir())
-            .map(|p| FullPath {
-                name: p
-                    .file_name()
-                    .and_then(|name| name.to_str().and_then(|name| Some(String::from(name))))
-                    .unwrap_or(String::from("")),
-                path: p
-                    .canonicalize()
-                    .and_then(|p| Ok(p.to_str().unwrap_or("").to_string()))
-                    .unwrap_or(String::from("")),
-                is_parent_node: false,
-            })
-            .filter(|p| p.name != "" && p.path != "")
-            .collect();
-
-        Ok([&parent_directory[..], &directories[..], &files[..]].concat())
-    }
-
-    fn create(&self, path: &str) -> Result<(), crate::repository::Error> {
-        Ok(match path.ends_with("/") {
-            true => fs::create_dir_all(path).and_then(|_| Ok(())),
-            false => fs::File::create(path).and_then(|_| Ok(())),
-        }?)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct FullPath {
-    pub name: String,
-    pub path: String,
-    pub is_parent_node: bool,
-}
-
-impl std::fmt::Display for FullPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}: {}", self.name, self.path)
-    }
-}
+use crate::repository::{FilePath, FilePathRepository};
 
 pub struct Dispatcher {}
 
@@ -97,6 +14,11 @@ impl Dispatcher {
     }
 }
 
+pub trait PathRepository {
+    fn list(&self, path: &str) -> Result<Vec<FullPath>, crate::repository::Error>;
+    fn create(&self, path: &str) -> Result<(), crate::repository::Error>;
+}
+
 pub trait Path {
     fn is_group_node(&self) -> bool;
     fn parent(&self) -> Option<String>;
@@ -104,28 +26,15 @@ pub trait Path {
     fn join(&self, path: &str) -> Result<String, crate::repository::Error>;
 }
 
-pub struct FilePath<'a> {
-    path: &'a std::path::Path,
+#[derive(Debug, Clone)]
+pub struct FullPath {
+    pub name: String,
+    pub path: String,
+    pub is_parent_node: bool,
 }
 
-impl<'a> Path for FilePath<'a> {
-    fn is_group_node(&self) -> bool {
-        self.path.is_dir()
-    }
-
-    fn parent(&self) -> Option<String> {
-        self.path
-            .parent()
-            .and_then(|p| p.to_str())
-            .and_then(|p| Some(p.to_string()))
-    }
-
-    fn canonicalize(&self) -> Result<String, crate::repository::Error> {
-        let fulll_path = self.path.to_path_buf().canonicalize()?;
-        Ok(fulll_path.to_str()?.to_string())
-    }
-
-    fn join(&self, path: &str) -> Result<String, crate::repository::Error> {
-        Ok(self.path.join(path).to_str()?.to_string())
+impl std::fmt::Display for FullPath {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}: {}", self.name, self.path)
     }
 }
