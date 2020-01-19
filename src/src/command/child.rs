e80640776a1ce1ebd383d4425caf0ea581fed0f8
notomo <notomo.motono@gmail.com>
2020-01-19 21:06:34 +0900

    Refactor `child`

--- src/src/command/child.rs
+++ src/src/command/child.rs
@@ -28,70 +28,53 @@ impl<'a> Command for ChildCommand<'a> {
                     .into_iter()
                     .map(|target| target.to_string())
                     .collect();
+
                 match (is_group_node, self.opts.layout) {
                     (false, _) => vec![self.opts.layout.leaf_node_action(paths)],
-                    (true, Layout::Open) => {
-                        let results: Vec<_> = paths
-                            .iter()
-                            .map(|p| (p, self.repository.list(&p)))
-                            .collect();
-
-                        let mut error_actions: Vec<_> = results
-                            .iter()
-                            .filter(|(_, res)| res.is_err())
-                            .map(|(p, res)| Action::ShowError {
-                                path: p.to_string(),
-                                message: res.as_ref().err().unwrap().inner.to_string(),
-                            })
-                            .collect();
-
-                        let mut actions: Vec<_> = results
-                            .into_iter()
-                            .filter(|(_, res)| res.is_ok())
-                            .flat_map(|(p, res)| {
-                                vec![
-                                    Paths::from(res.unwrap()).to_write_all_action(),
-                                    Action::TryToRestoreCursor {
-                                        path: p.to_string(),
-                                    },
-                                ]
-                            })
-                            .collect();
-
-                        error_actions.append(&mut actions);
-                        error_actions.push(Action::AddHistory {
+                    (true, Layout::Open) => paths
+                        .iter()
+                        .flat_map(|path| match self.repository.list(&path) {
+                            Ok(paths) => vec![
+                                Paths::from(paths).to_write_all_action(),
+                                Action::TryToRestoreCursor {
+                                    path: path.to_string(),
+                                },
+                            ],
+                            Err(err) => vec![Action::ShowError {
+                                path: path.to_string(),
+                                message: err.inner.to_string(),
+                            }],
+                        })
+                        .chain(vec![Action::AddHistory {
                             path: self.current.path.to_string(),
                             line_number: self.current.line_number,
                             back: false,
-                        });
-                        error_actions
-                    }
-                    _ => {
-                        let results: Vec<_> = paths
-                            .iter()
-                            .map(|p| (p, self.repository.list(&p)))
-                            .collect();
+                        }])
+                        .collect(),
+                    (true, _) => {
+                        let (items, errors) =
+                            paths
+                                .iter()
+                                .fold((vec![], vec![]), |(mut items, mut errors), path| {
+                                    match self.repository.list(&path) {
+                                        Ok(paths) => {
+                                            let item = Paths::from(paths).to_fork_buffer_item(path);
+                                            items.push(item);
+                                        }
+                                        Err(err) => errors.push(Action::ShowError {
+                                            path: path.to_string(),
+                                            message: err.inner.to_string(),
+                                        }),
+                                    };
+                                    (items, errors)
+                                });
 
-                        let mut actions: Vec<_> = results
-                            .iter()
-                            .filter(|(_, res)| res.is_err())
-                            .map(|(p, res)| Action::ShowError {
-                                path: p.to_string(),
-                                message: res.as_ref().err().unwrap().inner.to_string(),
-                            })
-                            .collect();
-
-                        let items: Vec<_> = results
-                            .into_iter()
-                            .filter(|(_, res)| res.is_ok())
-                            .map(|(p, res)| Paths::from(res.unwrap()).to_fork_buffer_item(p))
-                            .collect();
-
-                        actions.push(Action::ForkBuffer {
+                        let mut actions = vec![Action::ForkBuffer {
                             items: items,
                             mod_name: SplitModName::No,
                             split_name: SplitName::from(self.opts.layout),
-                        });
+                        }];
+                        actions.extend(errors);
                         actions
                     }
                 }
